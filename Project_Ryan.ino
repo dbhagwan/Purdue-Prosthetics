c560357de99f9c63396c8a3052dd6817ff1ea1d5
dbhagwan <56458622+dbhagwan@users.noreply.github.com>
2020-11-08 00:16:33 +0800

    Update Project_Ryan.ino
    LED test

--- Project_Ryan.ino
+++ Project_Ryan.ino
@@ -5,25 +5,27 @@ Servo servo1; // Linear Actuator 1
 Servo servo2; // Linear Actuator 2
 Servo servo3; // Linear Actuator 3
 
-// Initialize Pins
-const int buttonPin = 7; // Pushbutton connected to digital pin 7
-const int potPin = A0; // Potentiometer connected to analog pin A0
-const int myoPin = 5; // Myoelectic sensor connectected to digital pin 5
-const int analogOutPin = 3; // Analog output pin that the LED is attached to
+// Variables
+const int buttonPin = 7; // Digital pin connected to pushbutton 
+const int potPin = A0; // Analog pin connected to potentiometer 
+const int myoPin = A5; // Analog pin connected to Myoelectic sensor 
+const int analogOutPin = 3; // Analog output pin connected to output
 const int servoPin1 = 9; // Input pin of linear actuator 1
 const int servoPin2 = 10; // Input pin of linear actuator 2
 const int servoPin3 = 11; // Input pin of linear actuator 3
+int potValue = 0; // Value read from the potentiometer
+int myoValue = 0; // Value read from the Myoelectic sensor
+int outputValue = 0; // Value output to the PWM (analog out)
+int buttonState = 0; // State of the button
+int state = 0;
+int threshold = 700; // Threshold value of the Myoelectric sesnor
 
-
-// Initialize Variables
-int potValue; // Value read from the potentiometer
-int myoValue; // Value read from the Myoelectic sensor
-int outputValue; // Value output to the PWM (analog out)
-int buttonState; // State of the button
-int state;
+const int LED = 13;
 
 void reset()
 {
+  digitalWrite(LED, LOW);
+  
   // Sets actuators to a default relaxed position
   myoValue = 1023;
   servo1.write(myoValue);
@@ -40,32 +42,30 @@ void setup()
   servo2.attach(servoPin2); // Actuator 2 to pin 10
   servo3.attach(servoPin3);  // Actuator 3 to pin 11
 
+  pinMode(LED, OUTPUT);
+
   Serial.begin(9600);
 }
 
 void loop()
 {
-  buttonState = digitalRead(buttonPin); // Reads the state of the push button
-
-  if (buttonState)
-  {
+ // buttonState = digitalRead(buttonPin); // Reads the state of the push button
+ 
     potValue = analogRead(potPin); // Reads the value of the potentiometer
     //Serial.println(potValue);
 
-    if (potValue >= 0)
+    if (potValue >= 50)
     {
-      myoValue = analogRead(myoPin); // Reads the value of the potentiometer (value between 0 and 1023)
-
-      if (myoValue >= 700 && state == 0)
+     // myoValue = analogRead(myoPin); // Reads the value of the potentiometer (value between 0 and 1023)
+        myoValue  = 20;
+      if (myoValue >= threshold && state == 0)
       {
+        digitalWrite(LED, HIGH);
         for (int i = 0; i < 360; i++)
         {
           servo1.write(i);
           servo2.write(i);
           servo3.write(i);
-          //servo1.write(360-i);
-          //servo2.write(360-i);
-          //servo3.write(360-i);
           delay(10);
         }
 
@@ -75,7 +75,7 @@ void loop()
 
       //Serial.println(val);
 
-      if (myoValue >= 700 && state == 1)
+      if (myoValue >= threshold && state == 1)
       {
         outputValue = map(myoValue, 0, 1023, 0, 255);
         analogWrite(analogOutPin, outputValue);
@@ -85,23 +85,12 @@ void loop()
           servo1.write(i);
           servo2.write(i);
           servo3.write(i);
-          //servo1.write(1+i);
-          //servo2.write(1+i);
-          //servo3.write(1+i);
           delay(10);
         }
 
         delay(1000);
         state = 0;
-        //if (analogRead(myo) >= 800)
-        //{
-        //  state = 0;
-        // }  //}
-        // state = 0;
-
       }
-      Serial.println(state);
-
     }
 
     else
@@ -110,5 +99,4 @@ void loop()
     }
 
     //Serial.println(myoValue);
-  }
 }
